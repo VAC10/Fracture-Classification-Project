2a9c4a29adf6aadab01edb01a8b6f1400dfe2c4c
Vefa Asım Çayırlı <81007065+VAC10@users.noreply.github.com>
2024-05-06 17:56:01 +0300

    Update preprocess.py

--- Data Preprocessing and Normalization/preprocess.py
+++ Data Preprocessing and Normalization/preprocess.py
@@ -1,23 +1,31 @@
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from sklearn.model_selection import train_test_split
+import numpy as np
+import os
+
 
 # Resimleri ve etiketlerini yüklemek için kullanılacak listelerin tanımlanması
-images = []
-labels = []
+images_fracture = []
+labels_fracture = []
+images_non_fracture = []
+labels_non_fracture = []
 
 # Kırık resimleri yükleme ve etiketleme
 for filename in os.listdir(fracture_dir):
     img = load_img(os.path.join(fracture_dir, filename), target_size=(224, 224))  # Resmi yükleme ve boyutlandırma
     img_array = img_to_array(img) / 255.0  # Resmi numpy dizisine dönüştürme ve normalize etme işlemi
-    images.append(img_array)  # Resmi listeye ekleme
-    labels.append(1)  # Kırık olduğu belirtilen etiketi ekleme
+    images_fracture.append(img_array)  # Resmi listeye ekleme
+    labels_fracture.append(1)  # Kırık olduğu belirtilen etiketi ekle
 
 # Kırık olmayan resimleri yükleme ve etiketleme
 for filename in os.listdir(non_fracture_dir):
     img = load_img(os.path.join(non_fracture_dir, filename), target_size=(224, 224))
     img_array = img_to_array(img) / 255.0  
-    images.append(img_array)  # Resmi listeye ekleme
-    labels.append(0)  # Kırık olmadığını belirten etiketi ekleme
+    images_non_fracture.append(img_array)  # Resmi listeye ekle
+    labels_non_fracture.append(0)  # Kırık olmadığını belirten etiketi ekle
 
 # Resim ve etiket listelerini numpy dizilerine (tensör) dönüştürme
-X = np.array(images)
-y = np.array(labels)
+X_fracture = np.array(images_fracture)
+y_fracture = np.array(labels_fracture)
+X_non_fracture = np.array(images_non_fracture)
+y_non_fracture = np.array(labels_non_fracture)
+
